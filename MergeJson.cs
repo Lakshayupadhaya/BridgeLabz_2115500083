5f5ea6ba74ea1f4da33c1401dc1602d2b30de5bf
Lakshayupadhaya <lakshayupadhaya24@gmail.com>
2025-02-26 23:42:17 +0530

    [Lakshay]Add. Added programs on json and data

---
+++ MergeJson.cs
@@ -0,0 +1,19 @@
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+using Newtonsoft.Json.Linq; 
+namespace JSONassignment 
+{ 
+ class MergeJson 
+ { 
+ static void Main() 
+ { 
+ JObject json1 = JObject.Parse("{ \"name\": \"Lakshay\", \"age\": 25 }"); 
+ JObject json2 = JObject.Parse("{ \"email\": \"Lakshayupadhaya24@gmail..com\", \"city\": \"MTJ\" }"); 
+ json1.Merge(json2, new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Union }); 
+ Console.WriteLine(json1.ToString());
+
+ }  } 
+} 
